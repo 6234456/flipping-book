152d9549de62a7a524e206f5988020ede543f58f
6234456 <6234456@sina.com>
2026-05-16 12:43:23 +0200

    feat(primitives): add barrel export

---
+++ src/atlas-ui/primitives/index.ts
@@ -0,0 +1,31 @@
+export { Icon } from './Icon';
+export type { IconProps, IconSize } from './Icon';
+
+export { Button } from './Button';
+export type { ButtonProps, ButtonVariant, ButtonSize } from './Button';
+
+export { Toggle } from './Toggle';
+export type { ToggleProps } from './Toggle';
+
+export { Chip } from './Chip';
+export type { ChipProps } from './Chip';
+
+export { EmptyState } from './EmptyState';
+export type { EmptyStateProps } from './EmptyState';
+
+export { Tooltip, TooltipProvider } from './Tooltip';
+export type { TooltipProps } from './Tooltip';
+
+export { ToastProvider, useToast } from './Toast';
+export type { ToastVariant } from './Toast';
+
+export { DrawerHeader } from './DrawerHeader';
+export type { DrawerHeaderProps } from './DrawerHeader';
+
+export { Pin } from './Pin';
+export type { PinProps, PinStatus } from './Pin';
+
+export { Callout } from './Callout';
+export type { CalloutProps, CalloutVariant } from './Callout';
+
+export { MOTION } from './motion';
